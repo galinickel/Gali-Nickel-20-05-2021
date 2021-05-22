33a5de5836ca603f87fbe5c8dd121cde81233644
Gali Nickel <gali.nickel95@gmail.com>
2021-05-22 22:04:05 +0300

    set night mode as default

--- src/store/reducers/modeReducer.js
+++ src/store/reducers/modeReducer.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (state = true, action) => {
+export default (state = false, action) => {
     switch (action.type) {
         case 'TOGGLE_MODE':
             return !state
