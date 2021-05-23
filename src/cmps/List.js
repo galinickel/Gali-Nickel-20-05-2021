df9410a50d4180e32ba8c93da739d9607fd53b9b
Gali Nickel <gali.nickel95@gmail.com>
2021-05-23 13:10:49 +0300

    adjustment in list rendering

--- src/cmps/List.js
+++ src/cmps/List.js
@@ -7,19 +7,19 @@ import ListItem from './ListItem'
 const List = props => {
     const dispatch = useDispatch()
     const cityList = useSelector((state) => state.cityList)
-    const favoriteCities = useSelector((state)=>state.favoriteCities)
     const [favoritesToDisplay, setFavoritesToDisplay] = useState([])
     const { listType } = props
 
     useEffect(() => {
-        dispatch(getFavorites()).then(res => {
+        dispatch(getFavorites()).then(res=>{
             setFavoritesToDisplay(res.payload)
         })
-    }, [favoritesToDisplay.length])
+    }, [])
 
-    const toggleFavoriteCity = city => {
-        dispatch(toggleFavorite(city))
-        setFavoritesToDisplay([])
+    const toggleFavoriteCity = async (city) => {
+        await dispatch(toggleFavorite(city))
+        const res = await dispatch(getFavorites())
+        setFavoritesToDisplay(res.payload)
     }
 
     const renderContents = renderArr => {
@@ -27,17 +27,17 @@ const List = props => {
         return renderArr.map(city => {
             const isInFavorites = favoritesToDisplay.find((favoriteCity) => favoriteCity.Key === city.Key)
             return <ListItem
-            isInFavorites={isInFavorites}
-            city={city}
-            listType={listType}
-            toggleFavoriteCity={toggleFavoriteCity}
-            key={city.Key}
-            />       
+                isInFavorites={isInFavorites}
+                city={city}
+                listType={listType}
+                toggleFavoriteCity={toggleFavoriteCity}
+                key={city.Key}
+            />
         })
     }
     return (<div className={listType === 'favorites' ? 'ui container' : null}>
         <div className={listType === 'favorites' && favoritesToDisplay.length ? 'ui relaxed grid ' : 'ui divided list'}>
-            {listType === 'searchOptions'  && renderContents(cityList)}
+            {listType === 'searchOptions' && renderContents(cityList)}
             {listType === 'favorites' && renderContents(favoritesToDisplay)}
             {listType === 'favorites' && !favoritesToDisplay.length && <div className="favorites-empty-msg"> <p>
                 It seems awfully empty in here! Why not search some cities, and add them to your favorites?</p></div>}
